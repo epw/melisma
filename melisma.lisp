9e78e5dd0c4990f51663d725dac1c1f37ac32761
Eric Willisson <ericwillisson@gmail.com>
2016-06-19 23:59:20 -0400

    New, smarter system for note durations

--- melisma.lisp
+++ melisma.lisp
@@ -1,6 +1,7 @@
 (defpackage #:melisma
   (:use #:cl #:eric)
   (:export #:*input*
+	   #:make-voice
 	   #:make-note
 	   #:note
 	   #:render-lilypond
@@ -9,19 +10,36 @@
 (in-package #:melisma)
 
 (defvar *base-pitch*)
+(defvar *voice*)
+
+(defstruct voice
+  (instrument "piano")
+  (key "c \\major")
+  (time-sig "4/4")
+  (position 0)
+  (notes ()))
+(defun time-sig-lower (time-sig)
+  (parse-integer (subseq time-sig (1+ (position #\/ time-sig)))))
+(defun voice-time-sig-lower (voice)
+  (time-sig-lower (voice-time-sig voice)))
 
 (defstruct note
-  voice
-  duration
+  (voice *voice*)
+  beats
   pitch)
 
-(defun relative-note (pitch duration)
+(defun ensure-list (atom-or-list)
+  (if (listp atom-or-list) atom-or-list
+      (list atom-or-list)))
+
+(defun relative-note (pitch beats &optional (voice *voice*))
   (make-note :pitch (typecase pitch
 		      (nil nil)
 		      (list (mapcar (lambda (p) (+ p *base-pitch*)) pitch))
 		      (t (+ pitch *base-pitch*)))
-	     :duration duration))
-	     
+	     :beats beats
+	     :voice voice))
+
 (defun octave-marks (pitch)
   (if pitch
       (let ((octave (floor pitch 12)))
@@ -33,15 +51,42 @@
       (nth (mod pitch 12) (list 'c 'cis 'd 'dis 'e 'f 'fis 'g 'gis 'a 'ais 'b))
       'r))
 
+(defun render-note-value (beats time-sig-lower)
+  (multiple-value-bind (quotient remainder) (floor time-sig-lower beats)
+    (if (zerop remainder)
+	(format nil "~d" quotient)
+
+;; Make this second part handle (6 8) and (7 8). Be smarter than subtracting 1.
+	(let ((quotient (floor time-sig-lower (1- beats)))
+	      (remainder (render-note-value 1 time-sig-lower)))
+	  (format t "Remainder: ~a, every digit char: ~a, parsed: ~d, = quotient/2: ~a~%"
+		  remainder (every #'digit-char-p remainder) (parse-integer remainder)
+		  (= (parse-integer remainder) (2/ quotient)))
+	  (if (and (every #'digit-char-p remainder)
+		   (= (parse-integer remainder) (2* quotient)))
+	      (format nil "~d." quotient)
+	      (list (format nil "~d" quotient) remainder))))))
+    
+
+(defun render-duration (beats voice)
+  (if (< beats (beats-remaining-in-measure voice))
+      (render-note-value beats (voice-time-sig-lower voice))
+
+		   (render-note-value (beats-remaining-in-measure voice)
+				      (voice-time-sig-lower voice))
+		   ...)))
+      
 (defun render-note (note)
-  (if (and (note-pitch note) (listp (note-pitch note)))
-      (format nil "<~{~a~^ ~}>~a"
-	      (mapcar (lambda (p)
-			(format nil "~(~a~)~a" (render-pitch p) (octave-marks p)))
-		      (note-pitch note))
-	      (note-duration note))
-      (format nil "~(~a~)~a~a" (render-pitch (note-pitch note)) (octave-marks (note-pitch note))
-	      (note-duration note))))
+   (let ((note-value (if (and (note-pitch note) (listp (note-pitch note)))
+			 (mapcar (lambda (p)
+				   (format nil "~(~a~)~a" (render-pitch p) (octave-marks p)))
+				 (ensure-list (note-pitch note)))
+			 (list (render-pitch (note-pitch note)))))
+	 (durations (render-duration (note-beats note) (note-voice note))))
+     (with-output-to-string (s)
+       (dolist (duration durations)
+	 (format s "~a~{~a~^ ~}~a~a" (if (> (length note-value) 1) "<" "") note-value (if (> (length note-value) 1) "<" "")
+		 duration)))))
 
 (defun render (f notes)
   (format f "        {~%")
@@ -49,19 +94,20 @@
     (format f "    ~a~%" (render-note note)))
   (format f "        }~%"))
 
-(defun render-lilypond (instrument key tempo &rest piece)
+(defun render-lilypond (tempo &rest piece)
   (let ((piece-flattened (reverse (loop for note-or-list in piece nconc (if (listp note-or-list) note-or-list (list note-or-list))))))
     (with-output-to-string (s)
       (format s "\\version \"2.16.0\"
 \\score {")
-      (format s "
+      (let ((voices (remove-duplicates (mapcar #'note-voice piece-flattened))))
+	(dolist (voice voices)
+	  (format s "
   \\new Staff \\with {midiInstrument = #~s}
   {
     \\key ~(~a~)
-    \\tempo 4 = ~d~%" instrument key tempo)
-      (format s "      <<~%")
-      (let ((voices (remove-duplicates (mapcar #'note-voice piece-flattened))))
-	(dolist (voice voices)
+    \\tempo 4 = ~d~%" (voice-instrument voice) (voice-key voice) tempo)
+	  (format s "      <<~%")
+
 	  (render s (remove-if-not (lambda (note) (eq (note-voice note) voice)) piece-flattened))
 	  (unless (equal voice (car (last voices)))
 	    (format s "      \\\\~%"))))
