dd6e6f60a4e0edc7939b574037359795b9f4db41
Eric Willisson <ericwillisson@gmail.com>
2017-12-08 19:40:39 -0500

    Who even knows what this is

---
+++ tithe.lisp
@@ -0,0 +1,37 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package :eric)
+    (ql:quickload "eric"))
+  (unless (find-package :melisma)
+    (load (compile-file "/home/eric/projects/melisma/melisma.lisp"))))
+
+(defpackage #:tithe
+  (:use #:cl #:melisma)
+  (:export #:tithe))
+
+(in-package #:tithe)
+
+(defun tithe ()
+  (arrange-music 120 ((melody (make-voice :instrument "acoustic grand" :offset-note (octaves 1)))
+		     drums)
+
+    (n melody i-first) ; when
+    (n melody i-second) ; the
+    (n melody i-third 2) ; tithe
+    (n melody i-fourth 2) ; came
+    (n melody i-fifth) ; call-
+    (n melody i-first) ; -ing
+    (n melody i-first) ; where
+    (n melody i-second) ; were
+    (n melody i-first) ; you
+    ))
+;; (n melody i-sixth)
+    ;; (n melody i-fourth)
+    ;; (n melody (octaves -1 i-seventh))
+    ;; (n melody i-second)
+    ;; (n melody i-first 2)))
+
+(defun main ()
+  (play-lilypond (tithe)))
+
+(main)
+
