bdba22e32a10816013ae29aae29ec9de0b95aaf5
Eric Willisson <ericwillisson@gmail.com>
2019-03-21 23:43:15 -0400

    Sounds alright

--- understanding.lisp
+++ understanding.lisp
@@ -18,19 +18,31 @@
     (dotimes (_ 3)
       (n m (funcall triplet-modifier (third chord)) 1/2))))
 
+(defun jumping (m &optional (chord-modifier #'identity) (shift -1))
+  (let ((chord (funcall chord-modifier (diatonic-chord 1))))
+    (n m (first chord) 1)
+    (n m (second chord) 1/2)
+    (s m (octaves shift (third chord)) 1/2)))
+
 (defun understanding ()
   (arrange-music 120 ((m (make-voice :instrument +acoustic-grand+ :octave 2))
   		      (b (make-voice :instrument +acoustic-grand+)))
-
-    (melody-phrase m :triplet-modifier (lambda (note) (octaves -1 note)))
+    (jumping m)
     (n b (first (diatonic-chord 1)) 3)
+    (voice-catch-up m b)
     
-    (melody-phrase m :chord-modifier #'raise)
+    (jumping m #'raise 0)
     (n b (second (diatonic-chord 1)) 3)
+    (voice-catch-up m b)
 
-    (n b (third (diatonic-chord 1)) 3)
-
-    ;; (n b (first (diatonic-chord 1)) 3)
+    (let ((chord (raise (diatonic-chord 1))))
+      (n m (third chord) 1)
+      (n m (second chord) 1/2)
+      (s m (third chord) 1/2)
+      (n b (third (diatonic-chord 1)) 3)
+      (voice-catch-up m b)
+      
+      (n m (octaves 1 (second chord)) 2))
 
     ))
 					;    (voice-set-key m /A)
